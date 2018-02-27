4bb3c26b43cefe7d93dffff3211948911a5632f8
204Constie <kat.bana204@gmail.com>
2018-02-27 13:18:00 +0100

    get photos on scroll and append to masonry wrapper

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 /*
 
-    Use https://codewise-fe-api.herokuapp.com/photos endpoint, to get list of objects that contain photos data, e.g.:
-
     {
         "width":5616,
         "height":3744,
@@ -9,44 +7,50 @@
         "url":"https://unsplash.com/photos/N7XodRrbzS0/download"
     }
 
-    This endpoint accepts two query parameters: 'offset' and 'limit'. If you use endpoint like this:
-    https://codewise-fe-api.herokuapp.com/photos?offset=0&limit=5
-    it will return first 5 records from the database,
-
-    if you use it like this:
-    https://codewise-fe-api.herokuapp.com/photos?offset=10&limit=5
-    it will return records no. 10-14 end so on.
-
-    If you don't pass any parameters, offset is set to 0 end limit is set to 50.
-
     To use lightweight placeholder images instead of real photos, set truthy value for 'placeholders' parameter, e.g.
     https://codewise-fe-api.herokuapp.com/photos?placeholders=1, or
     https://codewise-fe-api.herokuapp.com/photos?offset=10&limit=5&placeholders=1
-
-    Good luck!
 
 */
 
 
 (function(window, document, $){
+'use strict';
+//variables
+var offsetCounter = 0;
+var limit = 15;
+var wrapper = $('#wrapper');
 
-function photosService(offset, limit){
-  var url = 'https://codewise-fe-api.herokuapp.com/photos?offset=' + offset + '&limit=' + limit;
+
+function photosService(){
+  var url = 'https://codewise-fe-api.herokuapp.com/photos?offset=' + (offsetCounter*limit) + '&limit=' + limit;
   $.get( url , function( data ) {
+    offsetCounter++;
     console.log('photosService result: ', data);
+    photosDisplayer();
   });
+};
+
+function photosDisplayer(){
+  console.log('photosDisplayer');
+
+  wrapper.append('a');
 };
 
 function infiniteScrollService(){
   $(window).scroll(function () {
-   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
-      //load new photos
+    // if ($(document).height() - win.height() == win.scrollTop())
+   if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
+      //load new photos from server
    }
-});
+ });
 };
 
 function init(){
-  photosService(0, 15);
+  photosService();
+  setTimeout(function(){
+     photosService();
+   }, 1000);
 };
 
 init();
