3b6ffdfd6f4d64831ef992b2fa43aa65efda6935
204Constie <kat.bana204@gmail.com>
2018-02-27 13:42:58 +0100

    uneven masonry working

--- app.js
+++ app.js
@@ -18,39 +18,61 @@
 'use strict';
 //variables
 var offsetCounter = 0;
-var limit = 15;
+var limit = 16;
 var wrapper = $('#wrapper');
+var windowElement = $(window);
+var documentElement = $(document);
+var col1 = $('#col1');
+var col2 = $('#col2');
+var col3 = $('#col3');
+var col4 = $('#col4');
 
 
 function photosService(){
-  var url = 'https://codewise-fe-api.herokuapp.com/photos?offset=' + (offsetCounter*limit) + '&limit=' + limit;
+  var url = 'https://codewise-fe-api.herokuapp.com/photos?offset=' + (offsetCounter*limit) + '&limit=' + limit + '&placeholders=1';
   $.get( url , function( data ) {
     offsetCounter++;
     console.log('photosService result: ', data);
-    photosDisplayer();
+    photosDisplayer(data);
   });
 };
 
-function photosDisplayer(){
-  console.log('photosDisplayer');
+function photosDisplayer(photos){
+  console.log('photosDisplayer: ', photos);
 
-  wrapper.append('a');
+  for(var i=0; i<photos.length; i += 4){
+    col1.append(
+      '<img class="masonry_img" src="' + photos[i].url + '" alt="Smiley face">' + '\n'
+    );
+    col2.append(
+      '<img class="masonry_img" src="' + photos[i+1].url + '" alt="Smiley face">' + '\n'
+    );
+    col3.append(
+      '<img class="masonry_img" src="' + photos[i+2].url + '" alt="Smiley face">' + '\n'
+    );
+    col4.append(
+      '<img class="masonry_img" src="' + photos[i+3].url + '" alt="Smiley face">' + '\n'
+    );
+  }
 };
 
 function infiniteScrollService(){
-  $(window).scroll(function () {
+  windowElement.scroll(function () {
     // if ($(document).height() - win.height() == win.scrollTop())
-   if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
+   if (windowElement.scrollTop() >= documentElement.height() - windowElement.height()) {
       //load new photos from server
+      console.log('infinite scroll');
+      photosService();
    }
  });
 };
 
 function init(){
   photosService();
-  setTimeout(function(){
-     photosService();
-   }, 1000);
+  infiniteScrollService();
+  // setTimeout(function(){
+  //    photosService();
+  //  }, 1000);
 };
 
 init();
